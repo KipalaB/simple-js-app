7bfe8032a14821dc4398156b06651294b0e97457
KipalaB <148507735+KipalaB@users.noreply.github.com>
2023-12-12 23:47:15 -0600

    updated for 1,2

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
-alert('Hello world');
+let pokemonList = [
+{ name : "Whimsicott", height : 0.7, type : ['Fairy','Grass'] },
+{ name : "Slyveon", height : 1, type : 'Fairy' },
+{ name : "Mimikyu", height : 0.2, type : ['Ghost', 'Fairy'] }
+]
 
-let favoriteFood = 'dirt'
-document.write(favoriteFood)
+
