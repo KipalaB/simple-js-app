5db89f264c1b46c711eb180047268ae94787bf82
KipalaB <148507735+KipalaB@users.noreply.github.com>
2024-01-06 12:31:41 -0600

    updated for 1.3

--- js/script.js
+++ js/script.js
@@ -4,4 +4,12 @@ let pokemonList = [
 { name : "Mimikyu", height : 0.2, type : ['Ghost', 'Fairy'] }
 ]
 
-
+for (let i=0; i < pokemonList.length; i++) {
+    document.write(pokemonList[i].name + " Height : " + pokemonList[i].height + "m")
+    if (pokemonList[i].height <.3) {
+        document.write(" - That's so tiny!")
+    }
+    if (pokemonList[i].name == 'Whimsicott') {
+        document.write(" - The best pokemon.")
+    }
+}
