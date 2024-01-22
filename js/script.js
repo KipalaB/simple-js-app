f0693e1a03de0a385d6868c41456fffa881856aa
KipalaB <148507735+KipalaB@users.noreply.github.com>
2024-01-21 20:43:00 -0600

    Updated for 1.5

--- js/script.js
+++ js/script.js
@@ -1,15 +1,27 @@
-let pokemonList = [
-{ name : "Whimsicott", height : 0.7, type : ['Fairy','Grass'] },
-{ name : "Slyveon", height : 1, type : 'Fairy' },
-{ name : "Mimikyu", height : 0.2, type : ['Ghost', 'Fairy'] }
-]
+let pokemonRepository = (function (){
+    let pokemonList = [
+        { name : "Whimsicott", height : 0.7, type : ['Fairy','Grass'] },
+        { name : "Slyveon", height : 1, type : 'Fairy' },
+        { name : "Mimikyu", height : 0.2, type : ['Ghost', 'Fairy'] }
+        ]
+        
+        function add (pokemon) {
+            pokemonList.push(pokemon);
+        }
 
-for (let i=0; i < pokemonList.length; i++) {
-    document.write(pokemonList[i].name + " Height : " + pokemonList[i].height + "m")
-    if (pokemonList[i].height <.3) {
-        document.write(" - That's so tiny!")
+        function getAll () {
+            return pokemonList;
+        }
+
+        return {
+            add: add,
+            getAll: getAll
+        };
+})();
+
+pokemonRepository.getAll().forEach(function(pokemon) {
+    document.write("<p>" + pokemon.name + " || Height : " + pokemon.height + "m" + "</p>")
+    if (pokemon.name == 'Whimsicott'){
+        document.write(' - The best pokemon.')
     }
-    if (pokemonList[i].name == 'Whimsicott') {
-        document.write(" - The best pokemon.")
-    }
-}
+});
