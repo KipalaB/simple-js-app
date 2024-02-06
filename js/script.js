0eb4e39d13bf3bff5576cbebba4dedf7cd85412e
KipalaB <148507735+KipalaB@users.noreply.github.com>
2024-02-05 18:39:49 -0600

    Updated 1.6 (finally)

--- js/script.js
+++ js/script.js
@@ -23,7 +23,7 @@ let pokemonRepository = (function (){
             pkmnList.appendChild(listItem);
         }
         function showDetails(pokemon){
-            console.log(pokemon);
+            console.log(pokemon.name);
         }
         return {
             add: add,
